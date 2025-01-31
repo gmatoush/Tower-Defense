4183e93458fa06d38d2377e5b8ac7234ea742055
Matoush <gavin.matoush@westpoint.edu>
2025-01-31 11:05:35 -0500

    Updated enemy class to roate image and move across the screen following a waypoint

--- enemy.py
+++ enemy.py
@@ -1,17 +1,56 @@
 import pygame as pg
+from pygame.math import Vector2
+import math
 
 # Create an enemy class extending the pygame Sprite class
 class Enemy(pg.sprite.Sprite):
-    def __init__(self, pos, image):
+    def __init__(self, waypoints, image):
         pg.sprite.Sprite.__init__(self)
-        self.image = image
+        self.waypoints = waypoints
+        self.pos = Vector2(waypoints[0])
+        self.target_waypoint = 1
+        self.speed = 2
+        self.original_image = image
+        self.angle = 0
+        self.image = pg.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect.center = self.pos
 
     def update(self):
         self.move()
+        self.rotate()
 
     def move(self):
-        self.rect.x += 1
+        # Define a target waypoint
+        if self.target_waypoint < len(self.waypoints):
+            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.movement = self.target - self.pos
+        else:
+            # enemy has reached the end of the path
+            self.kill()
+
+        # calculate distance to target
+        dist = self.movement.length()
+        # check if remaining distance is greater than enemy speed
+        if dist >= self.speed:
+            self.pos += self.movement.normalize() * self.speed
+        else:
+            if dist != 0:
+                self.pos += self.movement.normalize() * dist
+            self.target_waypoint += 1
+        self.rect.center = self.pos
+
+
+    def rotate(self):
+        # calc distance to next waypoint
+        dist = self.target - self.pos
+
+        # calc angle from dist
+        self.angle = math.degrees(math.atan2(-dist[1], dist[0]))
+
+        # rotate image and update rectangle
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
 
     
